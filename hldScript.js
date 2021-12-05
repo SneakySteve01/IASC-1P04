018b1d6c393d6fdeee9431881dd8274f76d04e52
Stephen <68299019+SneakySteve01@users.noreply.github.com>
2021-12-05 15:40:32 -0500

    Update hldScript.js

--- hldScript.js
+++ hldScript.js
@@ -1,12 +1,21 @@
 var prototype = document.getElementById("prototype");
+var submitted = document.getElementById("submitted");
 var latest = document.getElementById("latest");
 
 function prototypeClicked() {
     prototype.style.display = "block";
+    latest.style.display = "none";
+    submitted.style.display = "none";
+}
+
+function submittedClicked() {
+    submitted.style.display = "block";
+    prototype.style.display = "none";
     latest.style.display = "none";
 }
 
 function latestClicked() {
     latest.style.display = "block";
     prototype.style.display = "none";
+    submitted.style.display = "none";
 }
